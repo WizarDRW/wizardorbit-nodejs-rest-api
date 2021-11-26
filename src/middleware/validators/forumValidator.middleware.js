ac4deb59410b9660005b2569042ca56ddecd9f3a
Burak <wizard1456@hotmail.com>
2021-11-26 23:02:51 +0300

    Thrown tags rule in forum validator

--- src/middleware/validators/forumValidator.middleware.js
+++ src/middleware/validators/forumValidator.middleware.js
@@ -12,18 +12,18 @@ exports.createValidateForum = [
         .withMessage("Short description is required")
         .isLength({ min: 30 })
         .withMessage('Must be at least 30 chars long')
-        .isLength({ max: 150 })
+        .isLength({ max: 550 })
         .withMessage('Short description can contain max 150 characters'),
     check('categories')
         .exists()
         .withMessage('Categories is required')
         .isLength({ min: 1 })
         .withMessage('Must be at least 1 arrays long'),
-    check('tags')
-        .exists()
-        .withMessage('Tags is required')
-        .isLength({ min: 1 })
-        .withMessage('Must be at least 1 arrays long'),
+    // check('tags')
+    //     .exists()
+    //     .withMessage('Tags is required')
+    //     .isLength({ min: 1 })
+    //     .withMessage('Must be at least 1 arrays long'),
 ]
 
 exports.updateValidateForum = [
